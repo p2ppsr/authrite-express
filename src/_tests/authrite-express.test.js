48cc0541945cbfb5ee00bab18d83e1ab3e02f888
Brayden Langley <brayden167@gmail.com>
2022-05-05 09:49:59 -0700

    Added the code that was missing

--- src/_tests/authrite-express.test.js
+++ src/_tests/authrite-express.test.js
@@ -1,3 +1,4 @@
+/* eslint-env jest */
 const bsv = require('bsv')
 const crypto = require('crypto')
 const sendover = require('sendover')
@@ -7,38 +8,36 @@ const createNonce = require('../utils/createNonce')
 const verifyNonce = require('../utils/verifyNonce')
 
 const mockRes = {
-    status: jest.fn(() => mockRes),
-    json: jest.fn(() => mockRes)
+  status: jest.fn(() => mockRes),
+  json: jest.fn(() => mockRes)
 }
 
 const mockReq = {
-    body: {message: 'Hello'},
+  body: { message: 'Hello' }
 }
 
 const TEST_CLIENT_PRIVATE_KEY = '0d7889a0e56684ba795e9b1e28eb906df43454f8172ff3f6807b8cf9464994df'
 const TEST_SERVER_PRIVATE_KEY = '6dcc124be5f382be631d49ba12f61adbce33a5ac14f6ddee12de25272f943f8b'
 
 describe('authrite', () => {
-    beforeEach(() => {
+  beforeEach(() => {
+  })
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+  it('creates a verifiable 32 byte nonce :)', async () => {
+    const nonce = createNonce(TEST_SERVER_PRIVATE_KEY)
+    expect(verifyNonce(nonce, TEST_SERVER_PRIVATE_KEY)).toEqual(true)
+  })
+  it('recognizes a false 32 byte nonce :(', async () => {
+    const falseNonce = crypto.randomBytes(32).toString('base64')
+    expect(verifyNonce(falseNonce, TEST_SERVER_PRIVATE_KEY)).toEqual(false)
+  })
+  it('responds properly to an initial request', async () => {
+    const middlewareFunction = middleware({
+      serverPrivateKey: TEST_SERVER_PRIVATE_KEY
     })
-    afterEach(() => {
-        jest.clearAllMocks()
-      })
-    it('creates a verifiable 32 byte nonce :)', async () => {
-        const nonce = createNonce(TEST_SERVER_PRIVATE_KEY)
-        expect(verifyNonce(nonce, TEST_SERVER_PRIVATE_KEY)).toEqual(true)
-    })
-    it('recognizes a false 32 byte nonce :(', async () => {
-        const falseNonce = crypto.randomBytes(32).toString('base64')
-        expect(verifyNonce(falseNonce, TEST_SERVER_PRIVATE_KEY)).toEqual(false)
-    })
-    it('responds properly to an initial request', async () => {
-        
-        const middlewareFunction = middleware({
-        serverPrivateKey: TEST_SERVER_PRIVATE_KEY
-        })
 
-        middlewareFunction(req, mockRes)
-
-    })
+    middlewareFunction(req, mockRes)
+  })
 })
--- src/index.js
+++ src/index.js
@@ -1,28 +1,80 @@
 const bsv = require('bsv')
 const crypto = require('crypto')
 const { getPaymentAddress, getPaymentPrivateKey } = require('sendover')
-
-const middleware = config => (req, res, next) =>
-{
-    // Generate a new server nonce to use for signing the response
-    const serverNonce = crypto.randomBytes(32).toString('base64')
-
-    // Temporarily save client request info for testing purposes
-    clientIdentityKey = fetchConfig.headers['X-Authrite-Identity-Key']
-    clientNonce = fetchConfig.headers['X-Authrite-Nonce']
-    clientSig = fetchConfig.headers['X-Authrite-Signature']
-    responseMessage = {
-    message: 'hello Authrite'
+const createNonce = require('./utils/createNonce')
+const verifyNonce = require('./utils/verifyNonce')
+const AUTHRITE_VERSION = '0.1'
+const middleware = config => (req, res, next) => {
+  if (req.path === config.initialRequestPath) {
+    if (AUTHRITE_VERSION !== req.body.authrite) {
+      return res.status(400).json({
+        error: 'Authrite version incompatible'
+      })
     }
-    const derivedPrivateKey = sendover.getPaymentPrivateKey({
+    const serverNonce = createNonce(config.serverPrivateKey)
+    const message = req.body.nonce + serverNonce
+    const derivedPrivateKey = getPaymentPrivateKey({
+      recipientPrivateKey: config.serverPrivateKey,
+      senderPublicKey: req.body.identityKey,
+      invoiceNumber: 'authrite message signature-' + req.body.nonce + ' ' + serverNonce,
+      returnType: 'hex'
+    })
+    const signature = bsv.crypto.ECDSA.sign(bsv.crypto.Hash.sha256(Buffer.from(message)), bsv.PrivateKey.fromHex(derivedPrivateKey))
+    return res.status(200).json({
+      authrite: '0.1',
+      messageType: 'initialResponse',
+      identityKey: bsv.PrivateKey.fromHex(config.serverPrivateKey).publicKey.toString(),
+      nonce: serverNonce,
+      certificates: [],
+      requestedCertificates: [],
+      signature: signature.toString()
+    })
+  }
+  if (AUTHRITE_VERSION !== req.headers['X-Authrite']) {
+    return res.status(400).json({
+      error: 'Authrite version incompatible'
+    })
+  }
+  if (verifyNonce(req.headers['X-Authrite-YourNonce'], config.serverPrivateKey) === false) {
+    return res.status(401).json({
+      error: 'show sum\' R.E.S.P.E.C.T.'
+    })
+  }
+  // When the server response comes back, validate the signature according to the specification
+  const signingPublicKey = getPaymentAddress({
+    senderPrivateKey: config.serverPrivateKey,
+    recipientPublicKey: req.headers['X-Authrite-Indentity-Key'],
+    invoiceNumber: 'authrite message signature-' + req.headers['X-Authrite-Nonce'] + ' ' + response.headers['X-Authrite-YourNonce'],
+    returnType: 'publicKey'
+  })
+  // 2. Construct the message for verification
+  const messageToVerify = req.body ? JSON.stringify(req.body):req.url
+  // 3. Verify the signature
+  const signature = bsv.crypto.Signature.fromString(
+    req.headers['X-Authrite-Signature']
+  )
+  const verified = bsv.crypto.ECDSA.verify(
+    bsv.crypto.Hash.sha256(Buffer.from(messageToVerify)),
+    signature,
+    bsv.PublicKey.fromString(signingPublicKey)
+  )
+  if (verified === false) {
+    res.status(401).json({
+      error: 'You didn\'t sign signatures'
+    })
+  }
+  req.authrite = {
+    identityKey: req.headers['X-Authrite-Identity-Key']
+  }
+  next()
+  const derivedPrivateKey = sendover.getPaymentPrivateKey({
     recipientPrivateKey: TEST_SERVER_PRIVATE_KEY,
     senderPublicKey: fetchConfig.headers['X-Authrite-Identity-Key'],
     invoiceNumber: 'authrite message signature-' + fetchConfig.headers['X-Authrite-Nonce'] + ' ' + serverNonce,
     returnType: 'hex'
-    })
-    const responseSignature = bsv.crypto.ECDSA.sign(
+  })
+  const responseSignature = bsv.crypto.ECDSA.sign(
     bsv.crypto.Hash.sha256(Buffer.from(JSON.stringify(responseMessage))),
     bsv.PrivateKey.fromHex(derivedPrivateKey))
 }
-
 module.exports = { middleware }
